f00d1481a52b97245730a2901fce3f7ffdadaca9
denarojordan <denarojordan@vt.edu>
2024-04-30 15:38:50 -0400

    Jordan Update

---
+++ Project4/src/FreeBlock.java
@@ -0,0 +1,55 @@
+/**
+ * Comment...
+ * 
+ * @author Daniel Madden
+ * @author Jordan DeNaro
+ * @version 04-21-2024
+ */
+public class FreeBlock {
+
+    private int begIndex;
+    private FreeBlock next;
+
+    /**
+     * constructor for free blocks
+     * 
+     * @param begIn
+     *            beginning index of the free block
+     */
+    public FreeBlock(int begIn) {
+        begIndex = begIn;
+        next = null;
+    }
+
+
+    /**
+     * 
+     * @return
+     *         returns the beginning index of the freeBlock
+     */
+    public int getBegIndex() {
+        return begIndex;
+    }
+
+
+    /**
+     * 
+     * @param free
+     *            the free block we want to be set next
+     * 
+     */
+    public void setNext(FreeBlock free) {
+        next = free;
+    }
+
+
+    /**
+     * 
+     * @return
+     *         returns the next freeblock
+     */
+    public FreeBlock getNext() {
+        return next;
+    }
+
+}
--- Project4/src/MemManager.java
+++ Project4/src/MemManager.java
@@ -6,52 +6,62 @@
  * @version 04-21-2024
  */
 public class MemManager {
-    byte [] mem;
-    
+    byte[] mem;
+    FreeBlock[] freeBlock;
 
     /**
      * constructor for the memory pool
+     * 
      * @param poolsize
-     * the size given for memory pool
+     *            the size given for memory pool
+     *            makes an array keeping track of free blocks
      */
-    MemManager(int poolsize){
+    MemManager(int poolsize) {
         mem = new byte[poolsize];
+        int freeSize = (int)Math.log(poolsize);
+        freeBlock = new FreeBlock[freeSize];
+        freeBlock[freeSize - 1].setNext(new FreeBlock(0));
     }
-    
 
     // Insert a record and return its position handle.
 
     // space contains the record to be inserted, of length size.
 
+
     Handle insert(byte[] space, int size) {
-        
+
     }
 
     // Return the length of the record associated with theHandle
 
+
     int length(Handle theHandle) {
-        
+
     }
 
     // Free a block at the position specified by theHandle.
 
     // Merge adjacent free blocks.
 
+
     void remove(Handle theHandle) {
-        
+
     }
 
-    // Return the record with handle posHandle, up to size bytes, by // copying it into space.
+    // Return the record with handle posHandle, up to size bytes, by // copying
+    // it into space.
 
     // Return the number of bytes actually copied into space.
 
+
     int get(byte[] space, Handle theHandle, int size) {
-        
+
     }
 
     // Dump a printout of the freeblock list
 
+
     void dump() {
-        
+
     }
 }
