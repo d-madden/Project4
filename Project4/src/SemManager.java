6ae7fe9aa6c3da5ae0d79dd873bfee007a2b7343
denarojordan <denarojordan@vt.edu>
2024-04-30 14:33:28 -0400

    did Main Method

--- Project4/src/SemManager.java
+++ Project4/src/SemManager.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * The class containing the main method.
  *
@@ -34,6 +38,47 @@ public class SemManager {
      */
     public static void main(String[] args) {
         // This is the main file for the program.
-        Seminar dum = new Seminar();
+        
+        //the inital size of the memory manager
+        int memSize = Integer.parseInt(args[0]);
+        
+        //is the initial size of the hash table
+        int hashSize = Integer.parseInt(args[1]);
+        
+        // the file containing the commands
+        File file = null;
+        // Attempts to open the file and scan through it
+        try {
+
+            // takes the first command line argument and opens that file
+            file = new File(args[2]);
+
+            // creates a scanner object
+            Scanner scanner = new Scanner(file);
+
+            // creates a command processor object
+            CommandProcessor cmdProc = new CommandProcessor();
+
+            // reads the entire file and processes the commands
+            // line by line
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                // determines if the file has more lines to read
+                if (!line.trim().isEmpty()) {
+                    cmdProc.processor(line.trim());
+                }
+            }
+            // closes the scanner
+            scanner.close();
+        }
+        // catches the exception if the file cannot be found
+        // and outputs the correct information to the console
+        catch (FileNotFoundException e) {
+            System.out.println("Invalid file");
+            e.printStackTrace();
+        }
+        
+        
     }
+
 }
