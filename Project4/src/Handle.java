38babfe5496b5b7fd923e8daf07c2f829302f7fb
denarojordan <denarojordan@vt.edu>
2024-04-30 14:48:39 -0400

    Handle

--- Project4/src/Handle.java
+++ Project4/src/Handle.java
@@ -6,7 +6,54 @@
  * @version 04-21-2024
  */
 public class Handle {
-    
-    
-    
+    private int ID;
+    private int start;
+    private int length;
+
+    /**
+     * 
+     * @param Id
+     *            the ID of the record stored
+     * @param st
+     *            the start of the record stored in the Mem Manager
+     * @param ln
+     *            the length of the record
+     */
+    public Handle(int Id, int st, int ln) {
+        ID = Id;
+        start = st;
+        length = ln;
+
+    }
+
+
+    /**
+     * 
+     * @return
+     *         returns the ID
+     */
+    public int getId() {
+        return ID;
+    }
+
+
+    /**
+     * 
+     * @return
+     *         returns the start of the record
+     */
+    public int getStart() {
+        return start;
+    }
+
+
+    /**
+     * 
+     * @return
+     *         returns the length of the record
+     */
+    public int getLength() {
+        return length;
+    }
+
 }
---
+++ Project4/src/HandleTest.java
@@ -0,0 +1,46 @@
+
+/**
+ * 
+ * @author Daniel Madden
+ * @author Jordan DeNaro
+ * @version 04-21-2024
+ */
+public class HandleTest extends student.TestCase {
+
+    Handle test;
+
+    /**
+     * sets up the test methods
+     */
+    public void setUp() {
+        test = new Handle(1, 4, 5);
+    }
+
+
+    /**
+     * tests the getID method
+     */
+    public void testGetID() {
+
+        assertEquals(test.getId(), 1);
+    }
+
+
+    /**
+     * tests the getStart method
+     */
+    public void testGetStart() {
+
+        assertEquals(test.getStart(), 4);
+    }
+
+
+    /**
+     * tests the getLength method
+     */
+    public void testGetLength() {
+
+        assertEquals(test.getLength(), 5);
+    }
+
+}
