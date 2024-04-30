acae982f53f9cbd296f07dab6b63502896b29883
denarojordan <denarojordan@vt.edu>
2024-04-30 15:21:33 -0400

    delete sem stuff

--- Project4/src/MemManager.java
+++ Project4/src/MemManager.java
@@ -1,33 +1,57 @@
-
+/**
+ * Comment...
+ * 
+ * @author Daniel Madden
+ * @author Jordan DeNaro
+ * @version 04-21-2024
+ */
 public class MemManager {
+    byte [] mem;
     
- // Constructor. poolsize defines the size of the memory pool in bytes 
 
-    MemManager(int poolsize);
+    /**
+     * constructor for the memory pool
+     * @param poolsize
+     * the size given for memory pool
+     */
+    MemManager(int poolsize){
+        mem = new byte[poolsize];
+    }
+    
 
     // Insert a record and return its position handle.
 
     // space contains the record to be inserted, of length size.
 
-    Handle insert(byte[] space, int size);
+    Handle insert(byte[] space, int size) {
+        
+    }
 
     // Return the length of the record associated with theHandle
 
-    int length(Handle theHandle);
+    int length(Handle theHandle) {
+        
+    }
 
     // Free a block at the position specified by theHandle.
 
     // Merge adjacent free blocks.
 
-    void remove(Handle theHandle);
+    void remove(Handle theHandle) {
+        
+    }
 
     // Return the record with handle posHandle, up to size bytes, by // copying it into space.
 
     // Return the number of bytes actually copied into space.
 
-    int get(byte[] space, Handle theHandle, int size);
+    int get(byte[] space, Handle theHandle, int size) {
+        
+    }
 
     // Dump a printout of the freeblock list
 
-    void dump();
+    void dump() {
+        
+    }
 }
--- Project4/src/SemManager.java
+++ Project4/src/SemManager.java
@@ -45,38 +45,10 @@ public class SemManager {
         //is the initial size of the hash table
         int hashSize = Integer.parseInt(args[1]);
         
-        // the file containing the commands
-        File file = null;
-        // Attempts to open the file and scan through it
-        try {
+        CommandProcessor cmd = new CommandProcessor();
+        cmd.process(memSize, hashSize, args[2]);
 
-            // takes the first command line argument and opens that file
-            file = new File(args[2]);
 
-            // creates a scanner object
-            Scanner scanner = new Scanner(file);
-
-            // creates a command processor object
-            CommandProcessor cmdProc = new CommandProcessor();
-
-            // reads the entire file and processes the commands
-            // line by line
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                // determines if the file has more lines to read
-                if (!line.trim().isEmpty()) {
-                    cmdProc.processor(line.trim());
-                }
-            }
-            // closes the scanner
-            scanner.close();
-        }
-        // catches the exception if the file cannot be found
-        // and outputs the correct information to the console
-        catch (FileNotFoundException e) {
-            System.out.println("Invalid file");
-            e.printStackTrace();
-        }
         
         
     }
