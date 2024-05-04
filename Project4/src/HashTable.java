b3595d5c09bb08044541691164f861d8dc825c7e
Dan <57277053+d-madden@users.noreply.github.com>
2024-05-04 17:57:19 -0400

    hash done except hashDelete

--- Project4/src/HashTable.java
+++ Project4/src/HashTable.java
@@ -6,15 +6,75 @@
  * @version 04-21-2024
  */
 public class HashTable {
-    
-    int size;
-    
-    // Hash Table constructor 
+
+    // The hash table size will always be a power of two.
+    // When hashing key value k (the ID value) h1(k) = k mod M
+    // which is simply the ID mod the table size,
+    // and h2(k) = (((k/M) mod (M/2)) ∗ 2) + 1.
+    // The key difference from what OpenDSA describes
+    // is that your hash tables must be extensible.
+
+    private int size;
+    private Handle[] hash;
+
+    // Hash Table constructor
     public HashTable(int hashSize) {
-        
+
         size = hashSize;
+        hash = new Handle[size];
     }
-    
-    
-    
+
+
+    public void hashInsert(Handle e) {
+
+        int id = e.getId();
+
+        int home; // Home position for e
+        int pos = home = id % size; // Init probe sequence
+
+        for (int i = 1; hash[pos] != null; i++) {
+            if (id == hash[pos].getId()) {
+                System.out.println("Duplicates not allowed");
+                return;
+            }
+            pos = i * ((((id / size) % (size / 2)) * 2) + 1);
+        }
+        hash[pos] = e;
+
+    }
+
+
+    public boolean hashSearch(Handle e) {
+
+        int id = e.getId();
+
+        int home; // Home position for K
+        int pos = home = id % size; // Initial position is the home slot
+        for (int i = 1; (id != (hash[pos]).getId())
+            && (hash[pos] != null); i++) {
+            
+            pos = i * ((((id / size) % (size / 2)) * 2) + 1); // Next on probe
+                                                             // sequence
+        }
+        if (id == (hash[pos]).getId()) { // Found it
+
+            e = hash[pos];
+
+            return true;
+        }
+        else {
+            return false; // K not in hash table
+        }
+    }
+
+
+    public void hashDelete() {
+
+    }
+
+
+    public int hashSize() {
+        return size;
+    }
+
 }
