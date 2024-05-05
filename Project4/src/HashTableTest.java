49b2d331a4f5e5e8c4fcc43ebb53598b519febef
Dan <57277053+d-madden@users.noreply.github.com>
2024-05-04 20:50:31 -0400

    cmt

--- Project4/src/HashTableTest.java
+++ Project4/src/HashTableTest.java
@@ -2,6 +2,10 @@ import student.TestCase;
 
 /**
  * tests hashtable
+ * 
+ * @author Daniel Madden
+ * @author Jordan DeNaro
+ * @version 04-21-2024
  */
 public class HashTableTest extends TestCase {
 
