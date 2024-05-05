682b7953d5686248764d111ad9492fe13d0be566
denarojordan <denarojordan@vt.edu>
2024-05-04 22:25:43 -0400

    Merge branch 'master' of https://github.com/d-madden/Project4.git

---
+++ Project4/src/Tombstone.java
@@ -0,0 +1,37 @@
+/**
+ * The class containing the tombstone 
+ *
+ * @author Daniel Madden
+ * @author Jordan DeNaro
+ * @version 04-21-2024
+ */
+public class Tombstone extends Handle {
+
+    private static Tombstone tomb = null;
+
+    /**
+     * constructor for tombstone
+     */
+    public Tombstone() {
+        super(-1, 0, 0);
+        // TODO Auto-generated constructor stub
+    }
+
+
+    /**
+     * 
+     * tombstone get instance
+     * 
+     * @return
+     *         returns the tomb
+     */
+    public static Tombstone getInstance() {
+
+        if (tomb == null) {
+            tomb = new Tombstone();
+        }
+
+        return tomb;
+    }
+
+}
---
+++ Project4/src/TombstoneTest.java
@@ -0,0 +1,25 @@
+/**
+ * The class containing the tombstone
+ *
+ * @author Daniel Madden
+ * @author Jordan DeNaro
+ * @version 04-21-2024
+ */
+public class TombstoneTest extends student.TestCase {
+    /**
+     * tests the tombstone utilization
+     */
+    public void testSingletonBehavior() {
+        // Reset the singleton instance for a clean start
+
+        // Test the creation of the instance.
+        Tombstone firstInstance = Tombstone.getInstance();
+        assertNotNull("The first instance should not be null", firstInstance);
+
+        // Test that subsequent calls return the same instance.
+        Tombstone secondInstance = Tombstone.getInstance();
+        assertSame(
+            "The second instance should be the same as the first instance",
+            firstInstance, secondInstance);
+    }
+}
